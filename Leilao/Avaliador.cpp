95cd55f2f647cef1275da535caec8aa5becaf2d6
Thaynara Lobo <loboo.thaynara@gmail.com>
2023-10-19 11:17:12 -0300

    feat: implementing the class to evaluate an auction

---
+++ Leilao/Avaliador.cpp
@@ -0,0 +1,11 @@
+#include "Avaliador.hpp"
+
+void Avaliador::avalia(Leilao leilao){
+    std::vector<Lance>lances = leilao.recuperaLances();
+    Lance ultimoLance = lances.back();
+    maiorValor = ultimoLance.recuperaValor();
+}
+
+float Avaliador::recuperaMaiorValor() const{
+    return maiorValor;
+}
